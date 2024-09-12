7fa493847cc3cb68722e81c91b642764681e3666
Tuomas Liikala <tuomas.liikala@kajapro.fi>
2024-09-12 14:17:29 +0300

    3.1 puhelinluettelon backend step1

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    "persons": [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": "1"
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": "2"
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": "3"
+      },
+      {
+        "id": "5c46",
+        "name": "Tuomas Liikala",
+        "number": "0405913064"
+      },
+      {
+        "id": "0f3f",
+        "name": "Hätänumero",
+        "number": "112"
+      }
+    ]
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
